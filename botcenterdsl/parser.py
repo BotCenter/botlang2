d8110e21e9b904cb39b22a5945e4e40aa41292e2
Sergio Maass <sergio@postcenter.io>
2016-08-23 13:41:24 -0300

    Big refactoring on parser to support code information for each node and improve readability

--- botcenterdsl/parser.py
+++ botcenterdsl/parser.py
@@ -3,10 +3,159 @@ import re
 from botcenterdsl.ast.ast import *
 
 
-class Parser(object):
-
+class SExpression(object):
+    """
+    https://en.wikipedia.org/wiki/S-expression
+    """
     OPENING_PARENS = ['(', '[', '{']
     CLOSING_PARENS = [')', ']', '}']
+
+    def to_ast(self):
+        raise NotImplementedError
+
+
+class Atom(SExpression):
+
+    def __init__(self, token, line):
+
+        self.code = token
+        self.start_line = line
+        self.end_line = line
+
+    def to_ast(self):
+
+        if self.code in ['true', '#t']:
+            return Val(True)
+        if self.code in ['false', '#f']:
+            return Val(False)
+        try:
+            return Val(int(self.code))
+        except ValueError:
+            try:
+                return Val(float(self.code))
+            except ValueError:
+                return self.string_or_symbol(self.code)
+
+    def string_or_symbol(self, token):
+
+        if token.startswith('"') and token.endswith('"'):
+            return Val(token[1:-1])
+        if token.startswith("'"):
+            return Val(token[1:])
+        return Id(token)
+
+
+class Tree(SExpression):
+
+    def __init__(self, children, code, start_line, end_line):
+
+        self.children = children
+        self.code = code
+        self.start_line = start_line
+        self.end_line = end_line
+
+    def to_ast(self):
+
+        first = self.children[0].code
+
+        if first == 'if':
+            return self.if_node()
+
+        if first == 'and':
+            return self.and_node()
+
+        if first == 'or':
+            return self.or_node()
+
+        if first == 'define':
+            return self.define_node()
+
+        if first == 'local':
+            return self.local_node()
+
+        if first == 'begin':
+            return self.begin_node()
+
+        if first == 'fun' or first == 'function':
+            return self.function_node()
+
+        if first == 'bot-node':
+            return self.bot_node()
+
+        if first == 'node-result':
+            return self.bot_result_node()
+
+        return self.application_node()
+
+    def if_node(self):
+        return If(
+            self.children[1].to_ast(),
+            self.children[2].to_ast(),
+            self.children[3].to_ast()
+        )
+
+    def and_node(self):
+        return And(
+            self.children[1].to_ast(),
+            self.children[2].to_ast()
+        )
+
+    def or_node(self):
+        return Or(
+            self.children[1].to_ast(),
+            self.children[2].to_ast()
+        )
+
+    def define_node(self):
+        return Definition(
+                self.children[1].code,
+                self.children[2].to_ast()
+            )
+
+    def local_node(self):
+        return Local(
+            [
+                Definition(d.children[0].code, d.children[1].to_ast())
+                for d in self.children[1].children],
+            self.children[2].to_ast()
+        )
+
+    def begin_node(self):
+        return BodySequence(
+            [s_expr.to_ast() for s_expr in self.children[1:]]
+        )
+
+    def function_node(self):
+        return Fun(
+            [identifier.code for identifier in self.children[1].children],
+            BodySequence(
+                [s_expr.to_ast() for s_expr in self.children[2:]]
+            )
+        )
+
+    def bot_node(self):
+        return BotNode(
+            [identifier.code for identifier in self.children[1].children],
+            BodySequence(
+                [s_expr.to_ast() for s_expr in self.children[2:]]
+            )
+        )
+
+    def bot_result_node(self):
+        return BotResult(
+            self.children[1].to_ast(),
+            self.children[2].to_ast(),
+            self.children[3].to_ast()
+        )
+
+    def application_node(self):
+        return App(
+            self.children[0].to_ast(),
+            [s_expr.to_ast() for s_expr in self.children[1:]]
+        )
+
+
+class Parser(object):
 
     @classmethod
     def parse(cls, code):
@@ -18,66 +167,120 @@ class Parser(object):
         if not balanced:
             raise SyntaxError('unbalanced parentheses')
 
-        s_expression_strings = cls.separate_sexpr_strings(code)
-        s_expressions = [
-            cls.string_to_sexpr(string) for string in s_expression_strings
-        ]
-        abstract_syntax_trees = [
-            cls.sexpr_to_ast(sexpr) for sexpr in s_expressions
-        ]
-
+        s_expressions = Parser(code).s_expressions()
+        abstract_syntax_trees = [s_expr.to_ast() for s_expr in s_expressions]
         return BodySequence(abstract_syntax_trees)
 
-    @classmethod
-    def separate_sexpr_strings(cls, string):
+    FIND_STRINGS_REGEX = re.compile('"[^"]*"')
 
-        string = string.strip()
-        s_expression_strings = []
-        stack = []
-        start_index = 0
+    def __init__(self, code):
+
+        self.code = code
+        self.strings = {}
+
+        string_index = 0
+        for match in self.FIND_STRINGS_REGEX.finditer(code):
+            identifier = '__REPLACED_STR__{0}'.format(string_index)
+            self.strings[identifier] = match.group(0)
+            self.code = self.code.replace(
+                match.group(0),
+                identifier
+            )
+            string_index += 1
+
+    def s_expressions(self):
+
+        return self.s_expressions_from_string(self.code)
+
+    def restore_code(self, code):
+
+        restored_code = code
+        for key, value in self.strings.items():
+            restored_code = restored_code.replace(key, value)
+        return restored_code
+
+    def restore_token(self, token):
+
+        restored_token = self.strings.get(token)
+        if restored_token is not None:
+            return restored_token
+        return token
+
+    def s_expressions_from_string(self, s_expr_string, current_line=1):
+
+        s_expressions = []
+        parens_stack = []
+        last_index = 0
         current_index = 0
 
-        for char in string:
-            if char in cls.OPENING_PARENS:
-                stack.append(char)
-            if char in cls.CLOSING_PARENS:
-                stack.pop()
-                if len(stack) == 0:
-                    s_expression_strings.append(
-                        string[start_index:current_index+1].strip()
+        for index, char in enumerate(s_expr_string):
+
+            if char in [' ', '\t', '\n'] and len(parens_stack) == 0:
+                token = s_expr_string[last_index:index].strip()
+
+                if len(token) > 0:
+                    s_expressions.append(
+                        Atom(self.restore_token(token), current_line)
                     )
-                    start_index = current_index + 1
-            current_index += 1
+                    last_index = index + 1
 
-        # Handle single atoms
-        if len(s_expression_strings) == 0 and current_index > 0:
-            s_expression_strings.append(string)
+            if char == '\n':
+                current_line += 1
 
-        return s_expression_strings
+            if char in SExpression.OPENING_PARENS:
+                parens_stack.append((index, current_line))
 
+            if char in SExpression.CLOSING_PARENS:
+                start_index, start_line = parens_stack.pop()
+                if len(parens_stack) == 0:
+                    code = self.restore_code(
+                        s_expr_string[start_index:index+1].strip()
+                    )
+                    s_expr = Tree(
+                        self.s_expressions_from_string(
+                            s_expr_string[start_index+1:index],
+                            start_line
+                        ),
+                        code,
+                        start_line,
+                        current_line
+                    )
+                    s_expressions.append(s_expr)
+                    last_index = index + 1
+
+            current_index = index + 1
+
+        if current_index > last_index:
+            token = s_expr_string[last_index:].strip()
+            if len(token) > 0:
+                s_expressions.append(
+                    Atom(self.restore_token(token), current_line)
+                )
+
+        return s_expressions
 
     @classmethod
     def balanced_parens(cls, string):
-        '''
+        """
         Returns a (<balanced>, <index>) tuple in which <balanced> is a
         boolean that represents whether <string> has balanced parentheses.
         If <balanced> is true, <index> is None. If <balanced> is false, <index>
         is an integer value indicating the index in <string> that caused the
         checking to fail.
-        '''
+        """
         stack = []
         i = 0
 
         for char in string:
-            if char in cls.OPENING_PARENS:
+            if char in SExpression.OPENING_PARENS:
                 stack.append(char)
 
-            if char in cls.CLOSING_PARENS:
+            if char in SExpression.CLOSING_PARENS:
                 if len(stack) == 0:
                     return False, i
 
-                paren_index = cls.CLOSING_PARENS.index(char)
-                if stack.pop() != cls.OPENING_PARENS[paren_index]:
+                paren_index = SExpression.CLOSING_PARENS.index(char)
+                if stack.pop() != SExpression.OPENING_PARENS[paren_index]:
                     return False, i
             i += 1
 
@@ -85,170 +288,3 @@ class Parser(object):
             return False, i
 
         return True, None
-
-    @classmethod
-    def string_to_sexpr(cls, string):
-
-        simplified_string = string\
-            .replace('{', '(')\
-            .replace('[', '(')\
-            .replace('}', ')')\
-            .replace(']', ')')
-
-        return cls.tokens_to_sexpr(cls.tokenize(simplified_string))
-
-    FIND_STRINGS_REGEX = re.compile('"[^"]*"')
-
-    @classmethod
-    def tokenize(cls, chars):
-        """
-        Converts string to list of tokens
-        """
-        strings = []
-        for match in cls.FIND_STRINGS_REGEX.finditer(chars):
-            strings.append(match.group(0))
-            chars = chars.replace(
-                match.group(0),
-                '__REPLACED_STR__{0}'.format(len(strings))
-            )
-
-        def restore_strings(token):
-            result = token
-            for index, string in enumerate(strings):
-                result = result.replace(
-                    '__REPLACED_STR__{0}'.format(index+1),
-                    string
-                )
-            return result
-
-        tokens = chars\
-            .replace('(', ' ( ')\
-            .replace(')', ' ) ')\
-            .replace(':', ' : ')\
-            .split()
-
-        return [restore_strings(token) for token in tokens]
-
-    @classmethod
-    def sexpr_to_ast(cls, sexpr):
-        """
-        :param sexpr: s-expression to transform to AST
-        :rtype: ASTNode
-        """
-
-        # Numbers and booleans
-        if (
-            isinstance(sexpr, int) or
-            isinstance(sexpr, float) or
-            isinstance(sexpr, bool)
-        ):
-            return Val(sexpr)
-
-        # Strings and symbols
-        if not isinstance(sexpr, list):
-            if sexpr.startswith('"') and sexpr.endswith('"'):
-                return Val(sexpr[1:-1])
-            if sexpr.startswith("'"):
-                return Val(sexpr[1:])
-            return Id(sexpr)
-
-        # Logic expressions
-        if sexpr[0] == 'if':
-            return If(
-                cls.sexpr_to_ast(sexpr[1]),
-                cls.sexpr_to_ast(sexpr[2]),
-                cls.sexpr_to_ast(sexpr[3])
-            )
-        if sexpr[0] == 'and':
-            return And(
-                cls.sexpr_to_ast(sexpr[1]),
-                cls.sexpr_to_ast(sexpr[2])
-            )
-        if sexpr[0] == 'or':
-            return Or(
-                cls.sexpr_to_ast(sexpr[1]),
-                cls.sexpr_to_ast(sexpr[2])
-            )
-
-        # Definition
-        if sexpr[0] == 'define':
-            return Definition(
-                sexpr[1],
-                cls.sexpr_to_ast(sexpr[2])
-            )
-
-        # Definitions sequence
-        if sexpr[0] == 'local':
-            return Local(
-                [Definition(d[0], cls.sexpr_to_ast(d[1])) for d in
-                 sexpr[1]],
-                cls.sexpr_to_ast(sexpr[2])
-            )
-
-        # Sequence of expressions
-        if sexpr[0] == 'begin':
-            return BodySequence(
-                [cls.sexpr_to_ast(sexpr) for sexpr in sexpr[1:]]
-            )
-
-        # Anonymous functions
-        if sexpr[0] == 'fun' or sexpr[0] == 'function':
-            return Fun(
-                sexpr[1],
-                BodySequence(
-                    [cls.sexpr_to_ast(sexpr) for sexpr in sexpr[2:]]
-                )
-            )
-
-        # Bot node (kind of function)
-        if sexpr[0] == 'bot-node':
-            return BotNode(
-                sexpr[1],
-                BodySequence(
-                    [cls.sexpr_to_ast(sexpr) for sexpr in sexpr[2:]]
-                )
-            )
-
-        # Bot node result
-        if sexpr[0] == 'node-result':
-            return BotResult(
-                cls.sexpr_to_ast(sexpr[1]),
-                cls.sexpr_to_ast(sexpr[2]),
-                cls.sexpr_to_ast(sexpr[3])
-            )
-
-        # Function application
-        return App(
-            cls.sexpr_to_ast(sexpr[0]),
-            [cls.sexpr_to_ast(sexpr) for sexpr in sexpr[1:]]
-        )
-
-    @classmethod
-    def tokens_to_sexpr(cls, tokens):
-        if len(tokens) == 0:
-            raise SyntaxError('unexpected end of expression')
-        token = tokens.pop(0)
-        if '(' == token:
-            l = []
-            while tokens[0] != ')':
-                l.append(cls.tokens_to_sexpr(tokens))
-            tokens.pop(0)  # pop off ')'
-            return l
-        elif ')' == token:
-            raise SyntaxError('unexpected )')
-        else:
-            return cls.atom(token)
-
-    @classmethod
-    def atom(cls, token):
-        if token in ['true', '#t']:
-            return True
-        if token in ['false', '#f']:
-            return False
-        try:
-            return int(token)
-        except ValueError:
-            try:
-                return float(token)
-            except ValueError:
-                return token
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,5 @@
 import unittest
-from botcenterdsl.parser import Parser
+from botcenterdsl.parser import Parser, SExpression, Tree
 
 
 class ParserTestCase(unittest.TestCase):
@@ -18,31 +18,6 @@ class ParserTestCase(unittest.TestCase):
         self.assertFalse(Parser.balanced_parens('{[[]}')[0])
         self.assertFalse(Parser.balanced_parens(')ab(')[0])
 
-    def test_string_to_sexpr(self):
-
-        self.assertEqual(Parser.string_to_sexpr('true'), True)
-
-        sexpr = Parser.string_to_sexpr('(and true false)')
-        self.assertEqual(sexpr, ['and', True, False])
-
-        sexpr = Parser.string_to_sexpr('(+ (- 3 2) (sum 1 2 3 4))')
-        self.assertEqual(sexpr, ['+', ['-', 3, 2], ['sum', 1, 2, 3, 4]])
-
-        sexpr = Parser.string_to_sexpr('(f (g (h 2 3 4) "hola") i)')
-        self.assertEqual(sexpr, ['f', ['g', ['h', 2, 3, 4], '"hola"'], 'i'])
-
-        sexpr = Parser.string_to_sexpr("""
-            {local
-                [ (a 3) (b 2) ]
-                (f a b)
-            }
-        """)
-        self.assertEqual(sexpr, ['local', [['a', 3], ['b', 2]], ['f', 'a', 'b']])
-
-        sexpr = Parser.string_to_sexpr('(define x "Holi, soy juanito")')
-        self.assertEqual(len(sexpr), 3)
-        self.assertEqual(sexpr[2], '"Holi, soy juanito"')
-
     def test_separate_sexpr_strings(self):
 
         code = """
@@ -50,11 +25,33 @@ class ParserTestCase(unittest.TestCase):
             (define f (x) (+ x 4))
             (f x)
         """
-        sexpr_strings = Parser.separate_sexpr_strings(code)
+        sexpr_strings = [
+            sexpr.code for sexpr in Parser(code).s_expressions()
+            ]
         self.assertEqual(len(sexpr_strings), 3)
         self.assertEqual(sexpr_strings[0], '(define x 3)')
         self.assertEqual(sexpr_strings[1], '(define f (x) (+ x 4))')
         self.assertEqual(sexpr_strings[2], '(f x)')
+
+    def test_function_arguments(self):
+
+        code = "(fun (n) (+ n 2))"
+        fun_expr = Parser(code).s_expressions()[0]
+        self.assertEqual(fun_expr.children[0].code, 'fun')
+
+        args_expr = fun_expr.children[1]
+        self.assertTrue(isinstance(args_expr, Tree))
+        self.assertEqual(len(args_expr.children), 1)
+        self.assertEqual(args_expr.children[0].code, 'n')
+
+        code = "(fun (m n) (+ m n))"
+        fun_expr = Parser(code).s_expressions()[0]
+        args_expr = fun_expr.children[1]
+
+        self.assertTrue(isinstance(args_expr, Tree))
+        self.assertEqual(len(args_expr.children), 2)
+        self.assertEqual(args_expr.children[0].code, 'm')
+        self.assertEqual(args_expr.children[1].code, 'n')
 
     def test_bot_sexpr(self):
 
@@ -67,9 +64,65 @@ class ParserTestCase(unittest.TestCase):
                 )
             )
         """
-        bot_sexpr = Parser.string_to_sexpr(code)
-        self.assertEqual(len(bot_sexpr), 3)
-        result_sexpr = bot_sexpr[2]
+        bot_sexpr = Parser(code).s_expressions()
+        self.assertEqual(len(bot_sexpr), 1)
+        bot_node_sexpr = bot_sexpr[0].children
+        self.assertEqual(len(bot_node_sexpr), 3)
+
+        result_sexpr = bot_node_sexpr[2].children
         self.assertEqual(len(result_sexpr), 4)
-        self.assertEqual(result_sexpr[0], 'node-result')
-        self.assertEqual(result_sexpr[3], 'end-node')
+        self.assertEqual(result_sexpr[0].code, 'node-result')
+        self.assertEqual(result_sexpr[3].code, 'end-node')
+
+    def test_lines_of_code(self):
+
+        code = """
+            (define x 3)
+            (define f (x) (+ x 4))
+            (f x)
+        """
+        sexpr = Parser(code).s_expressions()
+        self.assertEqual(len(sexpr), 3)
+        self.assertEqual(sexpr[0].start_line, 2)
+        self.assertEqual(sexpr[0].end_line, 2)
+        self.assertEqual(sexpr[1].start_line, 3)
+        self.assertEqual(sexpr[1].end_line, 3)
+        self.assertEqual(sexpr[2].start_line, 4)
+        self.assertEqual(sexpr[2].end_line, 4)
+
+        code1 = "(+ 3 4)"
+        sexpr = Parser(code1).s_expressions()
+        self.assertEqual(len(sexpr), 1)
+        self.assertEqual(sexpr[0].start_line, 1)
+        self.assertEqual(sexpr[0].end_line, 1)
+
+        code2 = """
+            (bot-node (data)
+                (node-result
+                    data
+                    (append "Holi, soy Botcito. " "Quien eres tu?")
+                    end-node
+                )
+            )
+        """
+        sexpr = Parser(code2).s_expressions()
+        node_sexpr = sexpr[0]
+        self.assertEqual(node_sexpr.start_line, 2)
+        self.assertEqual(node_sexpr.end_line, 8)
+
+        args_sexpr = node_sexpr.children[1]
+        result_sexpr = node_sexpr.children[2]
+        botcito_sexpr = result_sexpr.children[2]
+        end_node_sexpr = result_sexpr.children[3]
+
+        self.assertEqual(args_sexpr.start_line, 2)
+        self.assertEqual(args_sexpr.end_line, 2)
+
+        self.assertEqual(result_sexpr.start_line, 3)
+        self.assertEqual(result_sexpr.end_line, 7)
+
+        self.assertEqual(botcito_sexpr.start_line, 5)
+        self.assertEqual(botcito_sexpr.end_line, 5)
+
+        self.assertEqual(end_node_sexpr.start_line, 6)
+        self.assertEqual(end_node_sexpr.end_line, 6)
