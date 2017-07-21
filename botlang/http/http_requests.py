51f834943d10e1deb2165473f2f508d9b9d40e65
Sergio Maass <sergio@postcenter.io>
2017-07-21 18:38:35 -0300

    HTTP POST using JSON payload

--- botlang/http/http_requests.py
+++ botlang/http/http_requests.py
@@ -23,7 +23,7 @@ def http_get(url, headers=None):
     return build_response_dict(response)
 
 
-def http_post(url, data, headers=None):
+def http_post(url, json, headers=None):
 
-    response = requests.post(url, data, headers=headers)
+    response = requests.post(url, json=json, headers=headers)
     return build_response_dict(response)
