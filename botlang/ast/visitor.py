aac282a28191c841c5a969ee4742f5e4fbc4e49c
Sergio Maass <sergio@postcenter.io>
2017-09-13 16:38:09 -0300

    Implemented default Visitor methods

--- botlang/ast/visitor.py
+++ botlang/ast/visitor.py
@@ -1,100 +1,101 @@
+from botlang.ast.ast import *
+
+
 class Visitor(object):
     """
     AST visitor base class
     """
 
-    def visit_val(self, val_node, env):
-        """
-        Value expression
-        """
-        raise NotImplementedError
+    def visit_val(self, val_node: Val, env):
 
-    def visit_if(self, if_node, env):
-        """
-        'If' conditional
-        """
-        raise NotImplementedError
+        return val_node
 
-    def visit_cond(self, cond_node, env):
-        """
-        'Cond' conditional
-        """
-        raise NotImplementedError
+    def visit_list(self, literal_list: ListVal, env):
 
-    def visit_cond_predicate_clause(self, predicate_node, env):
-        """
-        'Cond' predicate clause node
-        """
-        raise NotImplementedError
+        return [
+            element.accept(self, env) for element in literal_list.elements
+        ]
 
-    def visit_cond_else_clause(self, else_node, env):
-        """
-        'Cond' else clause node
-        """
-        raise NotImplementedError
+    def visit_if(self, if_node: If, env):
 
-    def visit_and(self, and_node, env):
-        """
-        Logical 'and'
-        """
-        raise NotImplementedError
+        if_node.cond.accept(self, env)
+        if_node.if_true.accept(self, env)
+        if_node.if_false.accept(self, env)
+        return if_node
 
-    def visit_or(self, or_node, env):
-        """
-        Logical 'or'
-        """
-        raise NotImplementedError
+    def visit_cond(self, cond_node: Cond, env):
 
-    def visit_id(self, id_node, env):
-        """
-        Identifier (variable name)
-        """
-        raise NotImplementedError
+        for clause in cond_node.cond_clauses:
+            clause.accept(self, env)
+        return cond_node
 
-    def visit_fun(self, fun_node, env):
-        """
-        Function expression
-        """
-        raise NotImplementedError
+    def visit_cond_predicate_clause(
+            self,
+            predicate_node: CondPredicateClause,
+            env
+    ):
+        predicate_node.predicate.accept(self, env)
+        predicate_node.then_body.accept(self, env)
+        return predicate_node
 
-    def visit_app(self, app_node, env):
-        """
-        Function application
-        """
-        raise NotImplementedError
+    def visit_cond_else_clause(self, else_node: CondElseClause, env):
+        return else_node.then_body.accept(self, env)
 
-    def visit_body(self, body_node, env):
-        """
-        Sequence of expressions
-        """
-        raise NotImplementedError
+    def visit_and(self, and_node: And, env):
 
-    def visit_definition(self, def_node, env):
-        """
-        Definition
-        """
-        raise NotImplementedError
+        and_node.cond1.accept(self, env)
+        and_node.cond2.accept(self, env)
+        return and_node
 
-    def visit_local(self, local_node, env):
-        """
-        Local definition
-        """
-        raise NotImplementedError
+    def visit_or(self, or_node: Or, env):
 
-    def visit_module_definition(self, module_node, env):
-        """
-        Module definition
-        """
-        raise NotImplementedError
+        or_node.cond1.accept(self, env)
+        or_node.cond2.accept(self, env)
+        return or_node
 
-    def visit_module_import(self, require_node, env):
-        """
-        Module import
-        """
-        raise NotImplementedError
+    def visit_id(self, id_node: Id, env):
 
-    def visit_module_function_export(self, provide_node, env):
-        """
-        Module function's export
-        """
-        raise NotImplementedError
+        return id_node
+
+    def visit_fun(self, fun_node: Fun, env):
+
+        return fun_node.body.accept(self, env)
+
+    def visit_app(self, app_node: App, env):
+
+        app_node.fun_expr.accept(self, env)
+        for argument in app_node.arg_exprs:
+            argument.accept(self, env)
+        return app_node
+
+    def visit_body(self, body_node: BodySequence, env):
+
+        for expression in body_node.expressions:
+            expression.accept(self, env)
+        return body_node
+
+    def visit_definition(self, def_node: Definition, env):
+
+        return def_node.expr.accept(self, env)
+
+    def visit_local(self, local_node: Local, env):
+
+        for definition in local_node.definitions:
+            definition.accept(self, env)
+        local_node.body.accept(self, env)
+        return local_node
+
+    def visit_module_definition(self, module_node: ModuleDefinition, env):
+
+        return module_node.body.accept(self, env)
+
+    def visit_module_import(self, require_node: ModuleImport, env):
+
+        return require_node
+
+    def visit_module_function_export(
+            self,
+            provide_node: ModuleFunctionExport,
+            env
+    ):
+        return provide_node
