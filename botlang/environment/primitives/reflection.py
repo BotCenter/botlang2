8b72bf2498f3d97219ff237e11e20964cfa30728
Sergio Maass <sergio@botcenter.io>
2018-06-22 16:00:40 -0400

    Added more reflective primitives

--- botlang/environment/primitives/reflection.py
+++ botlang/environment/primitives/reflection.py
@@ -1,19 +1,36 @@
+from botlang.evaluation.values import FunVal
+
+
 class BotlangReflectionException(Exception):
     pass
 
 
-def get_node(environment, node_id):
-    node = environment.lookup(node_id)
+def reflect_get(environment, identifier):
+    return environment.lookup(identifier)
 
+
+def reflect_get_node(environment, node_id):
+    node = reflect_get(environment, node_id)
     from botlang import BotNodeValue
     if not isinstance(node, BotNodeValue):
         raise BotlangReflectionException(
             "'{}' is not a bot node".format(node_id)
         )
-
     return node
 
 
+def reflect_get_fun(environment, node_id):
+    fun = reflect_get(environment, node_id)
+    if not isinstance(fun, FunVal):
+        raise BotlangReflectionException(
+            "'{}' is not a function".format(node_id)
+        )
+    return fun
+
+
 REFLECTIVE_PRIMITIVES = {
-    'get-node': get_node
+    'get-node': reflect_get_node,
+    'reflect-get': reflect_get,
+    'reflect-get-fun': reflect_get_fun,
+    'reflect-get-node': reflect_get_node
 }
--- tests/test_reflection.py
+++ tests/test_reflection.py
@@ -1,10 +1,58 @@
 import unittest
 
 from botlang import BotlangErrorException
+from botlang.evaluation.values import FunVal
 from botlang.interpreter import BotlangSystem
 
 
 class BotlangTestCase(unittest.TestCase):
+
+    def test_get_value(self):
+
+        bot_code = """
+        (define val1 1)
+        (define val2 "hola")
+        (define val3 (fun (x) x))
+        (bot-node (ctx msg)
+            (node-result ctx (reflect-get msg) end-node)
+        )
+        """
+        result = BotlangSystem.bot_instance().eval_bot(bot_code, 'val1')
+        self.assertEqual(result.message, 1)
+
+        result = BotlangSystem.bot_instance().eval_bot(bot_code, 'val2')
+        self.assertEqual(result.message, 'hola')
+
+        result = BotlangSystem.bot_instance().eval_bot(bot_code, 'val3')
+        self.assertTrue(isinstance(result.message, FunVal))
+
+        with self.assertRaises(BotlangErrorException) as cm:
+            BotlangSystem.bot_instance().eval_bot(bot_code, 'val4')
+        self.assertTrue("name 'val4' is not defined" in str(cm.exception))
+
+    def test_get_function(self):
+
+        bot_code = """
+        (define fun1 (function (x) (* x x)))
+        (define fun2 4)
+        (bot-node (context message)
+            (node-result
+                context
+                ((reflect-get-fun message) 3)
+                end-node
+            )
+        )
+        """
+        result = BotlangSystem.bot_instance().eval_bot(bot_code, 'fun1')
+        self.assertEqual(result.message, 9)
+
+        with self.assertRaises(BotlangErrorException) as cm:
+            BotlangSystem.bot_instance().eval_bot(bot_code, 'fun3')
+        self.assertTrue("name 'fun3' is not defined" in str(cm.exception))
+
+        with self.assertRaises(BotlangErrorException) as cm:
+            BotlangSystem.bot_instance().eval_bot(bot_code, 'fun2')
+        self.assertTrue("'fun2' is not a function" in str(cm.exception))
 
     def test_get_node(self):
 
@@ -18,10 +66,9 @@ class BotlangTestCase(unittest.TestCase):
         ))
         (define node2 1)
         (bot-node (context)
-            ((get-node (input-message)) context)
+            ((reflect-get-node (input-message)) context)
         )
         """
-
         result = BotlangSystem.bot_instance().eval_bot(bot_code, 'node1')
         self.assertEqual(result.message, 'Nodo 1')
 
